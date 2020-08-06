4dc61f14d5bff70cc7a97af3267b74daf18b92f9
brunoPedrosa <brunofellipep@gmail.com>
2020-08-06 20:29:09 -0300

    finished burguer menu. need to add a remove to it

--- script.js
+++ script.js
@@ -1,18 +1,54 @@
-const toggleButton = document.querySelector(".toggleMenuContainer");
+const newSelector = (selector) => {
+  return document.createElement(selector);
+};
 
-const menuButtonOpened = document.createElement("div");
-constuButtonOpened.style.cssText =
-  "position:absolute; border: solid 1px; top: 0; height:200px; width:100px; z-index: 10; background-color:white";
+const style = (selector, ...styles) => {
+  return (selector.style.cssText = styles);
+};
 
-const nav = document.createElement("a");
-const navText = document.createTextNode("join");
-nav.setAttribute("href", "join.html");
-nav.appendChild(navText);
-menuButtonOpened.appendChild(nav);
+const appendSelector = (selector, newChild) => {
+  const getSelector = document.querySelector(selector);
+  getSelector.addEventListener("click", () => {
+    getSelector.appendChild(newChild);
+  });
+};
 
-toggleButton.addEventListener("mouseenter", () => {
-  // toggleButton.style.border = "5px dotted orange";
-  // console.log(toggleButton)
-  toggleButton.appendChild(menuButtonOpened);
-  console.log(toggleButton);
-});
+const constructNewElement = (el, elList, selector, attribute, classOfItems) => {
+  for (value in elList) {
+    const selectors = el.appendChild(newSelector(selector));
+    const text = document.createTextNode(elList[value]);
+    const appendText = selectors.appendChild(text);
+    const attributes = selectors.setAttribute(
+      attribute,
+      `${elList[value]}.html`
+    );
+    const classOfNavItems = selectors.classList.add(classOfItems);
+  }
+  return el;
+};
+
+const burguerMenu = newSelector("div");
+
+style(
+  burguerMenu,
+  "position:absolute; border: outset 3px; top: 0; height:200px; width:100px; z-index: 10; background-color:white; border-radius: 5%"
+);
+
+const nav = newSelector("div");
+
+style(
+  nav,
+  "font-weight: 500; text-transform: uppercase; display: flex; flex-direction: column; align-items: baseline; margin-top: 20px;"
+);
+
+constructNewElement(
+  nav,
+  ["join", "about", "artists", "contact"],
+  "a",
+  "href",
+  "menuBurguer"
+);
+
+burguerMenu.appendChild(nav);
+
+appendSelector(".toggleMenuContainer", burguerMenu);
